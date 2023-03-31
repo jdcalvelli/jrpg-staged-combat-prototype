468572ea32f7808bc3b5254e41cc4c42e7940582
cjgonza2 <camrong520@gmail.com>
2023-03-31 16:24:08 -0400

    Set up the "menu", The attack button now has the ability to select which limb to attack with, doesn't do anything else otherwise.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -6,12 +6,16 @@ var _playerMech: PlayerMech
 var _enemyMech: EnemyMech
 var _playerText: RichTextLabel
 
-#State Machine, So each 
-enum States {Round_Start, Player_Select, Player_Target, Enemy_Select, Enemy_Target, Damage_Calc}
+var attackButton: Button
+
+#State Machine, TBD
+#enum States {Round_Start, Player_Select, Player_Target, Enemy_Select, Enemy_Target, Damage_Calc}
 
 # Called when the node enters the scene tree for the first time.
 # this is start
 func _ready():
+	
+	#Player Insantiation.
 	# this is really not a good way to do this, should find a better one
 	_playerText = get_node("PlayerStats") #This finds us the Text node so we can update it.
 	# creates a new PlayerMech class and passes it's health and it's body parts. 
@@ -31,6 +35,9 @@ func _ready():
 	+ ("\nLeft Leg Health: %s" % _playerMech.mechHealth) \
 	+ ("\nRight Leg Health: %s") % _playerMech.mechHealth
 	
+	#Player Button Instation
+	attackButton = get_node("CommandButton") #get_node works by finding nodes in the same node tree. so in this case the command button has to be childed to the GameManager for this to work as far as I've been able to work out. 
+	_add_commands() #calls add command function. 
 		
 	_enemyMech = EnemyMech.new(
 		20,
@@ -56,6 +63,15 @@ func _ready():
 	_enemyMech.attack()
 	
 
+#Adds commands on start up for each limb on the player mech. 
+func _add_commands():
+	attackButton.add_item("Select Part to Attack With") #adds the initial command that just says hey pick a command lol
+	
+	for _partKey in _playerMech.mechParts: #Populates the option menu with each part of the player mech. 
+		attackButton.add_item(_playerMech.mechParts[_partKey].partName)
+	#Considerations, When a part is destroyed is it possible to remove an item?
+	#On the options button manual page there is a way to remove an item in the button based on it's index
+	#Does godot index from 0 or 1?
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # this is update
