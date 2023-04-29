955722fc4e507e46af0eb30b462433d93848666b
JD Calvelli <jdcalvelli@gmail.com>
2023-04-29 16:22:54 -0400

    adding ugliest combo capacity yet created by mankind

--- Scripts/Controllers/PlayerController.gd
+++ Scripts/Controllers/PlayerController.gd
@@ -2,6 +2,10 @@ class_name PlayerController
 
 extends Node2D
 
+var isGrabbing : bool
+var isAOE : bool
+var isCrit : bool
+
 # variables for player mech
 var PlayerMechModel: Mech = Mech.new(
 			{
@@ -35,6 +39,35 @@ func UnsequencePartAttack(part: MechPart):
 		ActionPoints += 1
 	
 func ResolveAttackSequence(opponent: Mech, tempCombatLog: RichTextLabel):
+	# this does the base damage for each thing
 	for part in AttackSequence:
 		PlayerMechModel.Attack(part, opponent, target)
 		print("attack did %s damage with %s" % [part.partDamage, part.partName])
+	# this part should determine the combo
+	# this is the ugliest thing ive ever written
+	# i could figure out how to make it better but i wont rn
+	if (AttackSequence[0].partName == "Left Arm"
+	and AttackSequence[1].partName == "Right Arm"
+	or AttackSequence[0].partName == "Right Arm"
+	and AttackSequence[1].partName == "Left Arm"):
+		print("arm combo")
+		isGrabbing = true
+		# stop the enemy from using that part for next turn
+	elif (AttackSequence[0].partName == "Left Leg"
+	and AttackSequence[1].partName == "Right Leg"
+	or AttackSequence[0].partName == "Right Leg"
+	and AttackSequence[1].partName == "Left Leg"):
+		print("leg combo")
+		isCrit = true
+		# do double damage and maybe lose next turn
+	elif (AttackSequence[0].partName == "Left Arm"
+	and AttackSequence[1].partName == "Right Leg"
+	or AttackSequence[0].partName == "Right Leg"
+	and AttackSequence[1].partName == "Left Arm"
+	or AttackSequence[0].partName == "Right Arm"
+	and AttackSequence[1].partName == "Left Leg"
+	or AttackSequence[0].partName == "Left Leg"
+	and AttackSequence[1].partName == "Right Arm"):
+		print("arm/leg combo")
+		isAOE = true
+		# spread the damage across parts (for the sake of it just random)
